f4c79f73293104e40db62f3a18db2dc4b888147e
tytyne <dusaflora2@gmail.com>
2019-03-05 17:19:24 +0200

    i will remove a space in config.py

--- config.py
+++ config.py
@@ -17,10 +17,6 @@ class Config:
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-
-
-  
-
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
